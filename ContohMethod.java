78e3d81643c472b8a7762eedc6e19cf04311dc53
Andrew Simatupang <2381023@unai.edu>
2024-09-22 10:38:19 +0700

    handson4

---
+++ ContohMethod.java
@@ -0,0 +1,12 @@
+public class ContohMethod {
+    public static int hitungluaspersegi(int sisi){
+        int luas = sisi * sisi;
+        return luas;
+    }
+
+    public static void main(String[] args) {
+        int sisi = 5;
+        int luas = hitungluaspersegi(sisi);
+        System.out.println("Luas persegi dengan sisi " + sisi + " adalah " + luas);
+    }
+}
